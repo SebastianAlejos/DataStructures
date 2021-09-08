de10d62c6f9596b7deaf4d024dcc4cfdf98b6514
Sebastian Alejos <bastialejos@hey.com>
2021-09-08 11:30:25 -0400

    Changed output of the binary search function

--- binarysearch.java
+++ binarysearch.java
@@ -2,29 +2,32 @@ import javax.swing.JOptionPane;
 
 public class binarysearch {
 
-	public static int binarySearch(int[] data, int value) {
+	public static String binarySearch(int[] data, int value) {
 		int min=0,
 			max=data.length-1,
-			avg;
+			avg,
+			iterations;
+	
+		iterations=0;
 		while(min<=max){
+			iterations+=1;
 			avg=(min+max)/2;
-            System.out.println("Searching in position: "+avg);
 			if(value==data[avg]) {
-				return avg;
+				return "The program completed "+ iterations+ " iterations. The number exists in the position "+avg;
 			}else if(value< data[avg]) {
 				max=avg-1;
 			}else {
 				min=avg+1;
 			}
 		}
-		return -1;
+		return "The number does not exists on the list";
 	}
 	
 	
 	public static void main(String[] args) {
 		int data[] = { 2, 3, 4, 10, 40 };
 		int value=Integer.parseInt(JOptionPane.showInputDialog("Value to seach: "));
-		System.out.println("The number "+value+" is located in position: "+binarySearch(data,value));
+		System.out.println(binarySearch(data,value));
 	}
 
 }
